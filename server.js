97958a4265c876fd713cbab24af2b2ce8ce55835
David Ha <David.ha0908@gmail.com>
2021-11-09 16:34:22 -0700

    tweaks

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ app.use('sait-cpnt262-a5-david.herokuapp.com/api', router)
 
 
 // ERROR handlers for API or HTML
-// app.use((req, res) => {
-//   if(req.url.startsWith('/api')){
-//     res.status(404)
-//     res.send({ error: 'File Not Found'})
-//   } else {
-//       res.status(404)
-//       res.redirect('/404.html')
-//   }
-// })
+app.use((req, res) => {
+  if(req.url.startsWith('/api')){
+    res.status(404)
+    res.send({ error: 'File Not Found'})
+  } else {
+      res.status(404)
+      res.redirect('/404.html')
+  }
+})
 
 // Start Server
 const PORT = process.env.PORT || 3000;
