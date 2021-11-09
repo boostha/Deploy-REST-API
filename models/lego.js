3f928cc9066b6950a7972bd1c0d765564b8ec1dd
David Ha <David.ha0908@gmail.com>
2021-11-09 16:10:27 -0700

    tweaks

--- models/lego.js
+++ models/lego.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 
 mongoose.connect(
-  process.env.MONGODB_URL,
+  process.env.MONGODB_URI,
   { useUnifiedTopology: true, useNewUrlParser: true },
   )
   .then(function(){
